f0badd9235373171ffbf52da651a5469b994100a
Emil Djupvik <emil14833@gmail.com>
2020-10-31 00:31:40 +0100

    add authentication

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+    }
+}
