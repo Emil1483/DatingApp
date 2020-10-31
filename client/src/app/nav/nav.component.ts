c889c20a781090bc16db0ac537ef1142ef1274c3
Emil Djupvik <emil14833@gmail.com>
2020-10-31 18:12:15 +0100

    add login and logout

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,14 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {}
+  loggedIn: boolean;
 
-  constructor() { }
+  constructor(private accountService: AccountService) {
+
+  }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(
+      response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error => {
+        console.log(error.error);
+      }
+      )
+  }
+
+  logout() {
+    this.loggedIn = false;
   }
 
 }
