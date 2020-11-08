6d48a0a6018e5495eab9b551b131b2b99991810d
Emil Djupvik <emil14833@gmail.com>
2020-11-08 20:46:46 +0100

    add user seed and mapping

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -20,11 +20,11 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Phots { get; set; }
+        public ICollection<Photo> Photos { get; set; }
 
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
