4d3b407edb3fd317c9cf2bafec90e0e80b96b8aa
Emil Djupvik <emil14833@gmail.com>
2020-11-10 17:32:47 +0100

    add photos tab

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 
 @NgModule({
@@ -14,11 +15,13 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
       positionClass: 'toast-bottom-right'
     }),
     TabsModule.forRoot(),
+    NgxGalleryModule
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
     TabsModule,
+    NgxGalleryModule,
   ]
 })
 export class SharedModule { }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { map } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -10,16 +12,38 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberDetailComponent implements OnInit {
   member: Member;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private membersService: MembersService, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    this.loadMember();
+  async ngOnInit(): Promise<void> {
+    await this.loadMember();
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false,
+      },
+    ];
+
+    this.galleryImages = this.member.photos.map(photo => {
+      return {
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+      };
+    });
   }
 
-  loadMember() {
-    this.membersService.getMember(this.route.snapshot.paramMap.get('username'))
-      .subscribe(member => this.member = member);
+  async loadMember() {
+    return this.membersService.getMember(this.route.snapshot.paramMap.get('username'))
+      .pipe(map(member => this.member = member))
+      .toPromise();
   }
 
 }
