4ec9889b70ce2acdea979bdd0778db285181fb1e
Emil Djupvik <emil14833@gmail.com>
2020-11-10 13:23:30 +0100

    add details route

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member;
 
-  constructor() { }
+  constructor(private membersService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.membersService.getMember(this.route.snapshot.paramMap.get('username'))
+      .subscribe(member => this.member = member);
   }
 
 }
